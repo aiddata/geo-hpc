847d4617464574efdaf802e3677fae362a1ac4c5
userx <sgoodman@aiddata.org>
2015-03-24 13:35:24 -0400

    Update from post processing of mosaic to pre processing of individual frames.

--- gimms_ndvi_hpc.sh
+++ gimms_ndvi_hpc.sh
@@ -9,6 +9,9 @@
 year=$1
 day=$2
 
+export GDAL_CACHEMAX=12287
+
+
 # update to use user's /local/scr directory on node
 myuser="sgoodman"
 
@@ -17,8 +20,12 @@ in_dir="/sciclone/data20/aiddata/REU/raw/gimms.gsfc.nasa.gov/MODIS/std/GMOD09Q1/
 tmp_dir="/local/scr/"${myuser}"/REU/data/gimms.gsfc.nasa.gov/MODIS/std/GMOD09Q1/tif/NDVI/"${year}/${day}
 out_dir="/sciclone/data20/aiddata/REU/data/gimms.gsfc.nasa.gov/MODIS/std/GMOD09Q1/tif/NDVI/"${year}/${day}
 
-# make and go to input unzip directory
+# make directories
 mkdir -p "$in_dir"/unzip
+mkdir -p "$tmp_dir"
+mkdir -p "$out_dir"
+
+
 cd "$in_dir"
 
 # gunzip each gzipped file to "unzip" directory if not already gunzipped
@@ -29,29 +36,48 @@ for a in *.gz; do
 	fi
 done
 
-# make output (and prep) directory
-# mkdir -p "$out_dir"/prep
-
-# prep each tif
-# multiply all values by 0.004
-# set values less than 0 or greater than 250 to NA value (255)
 cd unzip
-# for f in *; do
-# 	calc_in="$in_dir"/unzip/"$f"
-# 	calc_out="$out_dir"/prep/"$f"
-# 	gdal_calc.py -A $calc_in --outfile=$calc_out --calc="A*0.004" --calc="A*(A>=0)+(-9999)*(A<0)" --calc="A*(A<=250)+(-9999)*(A>250)" --NoDataValue=-9999
-# done
 
-mkdir -p "$tmp_dir"
-mkdir -p "$out_dir"
+
+# METHOD: tif mosaic from original tifs -> calc on mosaic tif -> output tif
+# add tmp files and checks to see if process was interrupted (tmp exists, actual does not)
+# skips if tmp does not exist and actual does (assumed process completed succesfully)
+
+# process individual frames
+for a in *.tif; do
+
+	prep_tmp="$tmp_dir"/"$a"
+	\rm -f "$prep_tmp"
+	gdal_calc.py -A "$a" --outfile="$prep_tmp" --calc="A*(A<=250)+(255)*(A>250)" --NoDataValue=255
+
+done
+
 
 # get a file name for the output mosiac
 # just use the name of the first tif you find in the dir, except without xNNyNN tile name
-
 outname=$( find . -type f -iregex ".*[.]tif" | sed -n "1p" | sed "s:x[0-9]\+y[0-9]\+[.]::g" | sed "s:^[.]/::g;s:^:mosaic.:g" )
 
-# data source
-# /sciclone/data20/aiddata/REU/data/gimms.gsfc.nasa.gov/MODIS/std/GMOD09Q1/tif/NDVI/2000/249/unzip
+cd "$tmp_dir"
+# build mosaic
+mosaic_tmp="$tmp_dir"/tmp_"$outname"
+mosaic_act="$out_dir"/"$outname"
+if [[ -f "$mosaic_tmp" || ! -f "$mosaic_act" ]]; then
+	
+	\rm -f "$mosaic_tmp"
+	\rm -f "$mosaic_act"
+
+	gdal_merge.py -of GTiff -init 255 -n 255 -co COMPRESS=LZW -co TILED=YES -co BIGTIFF=YES *.tif -o "$mosaic_tmp"
+
+	# move from tmp_dir to out_dir
+	mv "$mosaic_tmp" "$mosaic_act"
+
+	# clean up tmp_dir
+	\rm -f "$tmp_dir"/*
+
+fi
+
+
+
 
 
 
@@ -60,37 +86,37 @@ outname=$( find . -type f -iregex ".*[.]tif" | sed -n "1p" | sed "s:x[0-9]\+y[0-
 # skips if tmp does not exist and actual does (assumed process completed succesfully)
 
 # build mosaic
-pre_tmp="$tmp_dir"/tmp_raw_"$outname"
-pre_act="$out_dir"/raw_"$outname"
-if [[ -f "$pre_tmp" || ! -f "$pre_act" ]]; then
+# pre_tmp="$tmp_dir"/tmp_raw_"$outname"
+# pre_act="$out_dir"/raw_"$outname"
+# if [[ -f "$pre_tmp" || ! -f "$pre_act" ]]; then
 	
-	\rm -f "$pre_tmp"
-	\rm -f "$pre_act"
+# 	\rm -f "$pre_tmp"
+# 	\rm -f "$pre_act"
 
-	gdal_merge.py -of GTiff -init 255 -co COMPRESS=LZW -co TILED=YES -co BIGTIFF=YES *.tif -o "$pre_tmp"
+# 	gdal_merge.py -of GTiff -init 255 -co COMPRESS=LZW -co TILED=YES -co BIGTIFF=YES *.tif -o "$pre_tmp"
 
-	# copy from tmp_dir to out_dir
-	cp "$pre_tmp" "$pre_act"
+# 	# copy from tmp_dir to out_dir
+# 	cp "$pre_tmp" "$pre_act"
 
-fi
+# fi
 
-# process mosaic
-post_tmp="$tmp_dir"/tmp_"$outname"
-post_act="$out_dir"/"$outname"
-if [[ -f "$post_tmp" || ! -f "$post_act" ]]; then
+# # process mosaic
+# post_tmp="$tmp_dir"/tmp_"$outname"
+# post_act="$out_dir"/"$outname"
+# if [[ -f "$post_tmp" || ! -f "$post_act" ]]; then
 	
-	\rm -f "$post_tmp"
-	\rm -f "$post_act"
+# 	\rm -f "$post_tmp"
+# 	\rm -f "$post_act"
 
-	gdal_calc.py -A "$pre_tmp" --outfile="$post_tmp" --calc="A*(A<=250)+(255)*(A>250)" --NoDataValue=255
+# 	gdal_calc.py -A "$pre_tmp" --outfile="$post_tmp" --calc="A*(A<=250)+(255)*(A>250)" --NoDataValue=255
 
-	# move from tmp_dir to out_dir
-	mv "$post_tmp" "$post_act"
+# 	# move from tmp_dir to out_dir
+# 	mv "$post_tmp" "$post_act"
 
-	# clean up tmp_dir
-	\rm -f "$pre_tmp"
+# 	# clean up tmp_dir
+# 	\rm -f "$pre_tmp"
 
-fi
+# fi
 
 
 
--- run_scripts.py
+++ run_scripts.py
@@ -13,22 +13,25 @@ rank = comm.Get_rank()
 
 
 # get years
-path_base = "/sciclone/data20/aiddata/REU/data/gimms.gsfc.nasa.gov/MODIS/std/GMOD09Q1/tif/NDVI/"
+path_base = "/sciclone/data20/aiddata/REU/raw/gimms.gsfc.nasa.gov/MODIS/std/GMOD09Q1/tif/NDVI/"
 # years = [x[x.rindex('/')+1:] for x,y,z in os.walk(base) if x[x.rindex("/")+1:] != self]
 
 
 # for year in years:
 
-year = '2010'
+year = '2008'
 
 
 # get days for year
 path_year = path_base + year
 
-days =[ name for name in os.listdir(path_year) if os.path.isdir(os.path.join(path_year, name)) and name != year ]
+# days =[ name for name in os.listdir(path_year) if os.path.isdir(os.path.join(path_year, name)) and name != year ]
 
 # use limited days for testing 
 # days = ['001','009','017','025','033','041','049','057']
+# days = ['169','177','185','193']
+
+days = ['001','009','017','025']
 
 
 if len(days) > size:
