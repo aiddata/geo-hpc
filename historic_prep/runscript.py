e6dc5baa1fcf464603381981d50e60351f01fcec
userx <sgoodman@aiddata.org>
2015-04-02 15:31:37 -0400

    Fix bugs with historic prep.

--- historic_prep/runscript.py
+++ historic_prep/runscript.py
@@ -12,7 +12,7 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 
 
-def isNum(numStr, numType):
+def isInt(numStr, numType):
 	try:
 		i = int(numStr)
 		if numType == 'year' and i >= 1981 and i <= 2002:
@@ -29,14 +29,14 @@ def isNum(numStr, numType):
 path_base = "/sciclone/data20/aiddata/REU/raw/historic_gimms_ndvi"
 
 # list of all [year, file] combos
-qlist = [[name[14:18], name[18:20], name] for name in os.listdir(path_base) if not os.path.isdir(os.path.join(path_base, name)) and name.endswith('.asc') and isInt(name[14:18], 'year') and isInt(name[18:20]), 'month']
+qlist = [[name[14:18], name[18:20], name] for name in os.listdir(path_base) if not os.path.isdir(os.path.join(path_base, name)) and name.endswith('.asc') and isInt(name[14:18], 'year') and isInt(name[18:20], 'month')]
 
 
 c = rank
 while c < len(qlist):
 
 	try:
-		cmd = runscript+" "+qlist[c][0]+" "+qlist[c][1]+" "qlist[c][2]
+		cmd = runscript+" "+qlist[c][0]+" "+qlist[c][1]+" "+qlist[c][2]
 		sts = sp.check_output(cmd, stderr=sp.STDOUT, shell=True)
 		print sts
 
