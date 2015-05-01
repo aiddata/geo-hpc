328fd0677a157a5ef68965779ff31689bc8efac4
usery <sgoodman@aiddata.org>
2015-05-01 16:53:51 -0400

    Update test stuff.

--- runscript_test.py
+++ runscript_test.py
@@ -43,10 +43,10 @@ status = MPI.Status()
 
 # comm.Barrier()
 
+# structured based on https://github.com/jbornschein/mpi4py-examples/blob/master/09-task-pull.py
+
 def enum(*sequential, **named):
-    """Handy way to fake an enumerated type in Python
-    http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
-    """
+    # source: http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
     enums = dict(zip(sequential, range(len(sequential))), **named)
     return type('Enum', (), enums)
 
