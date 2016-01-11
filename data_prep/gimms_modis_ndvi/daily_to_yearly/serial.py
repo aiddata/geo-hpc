a3aa38a4dbcb4f5d597dcec25a1f7023c21b71a3
userz <sgoodman@aiddata.org>
2016-01-11 17:07:38 -0500

    Restructure data prep directory. Add nighttimelights intercalibration.

--- data_prep/gimms_modis_ndvi/daily_to_yearly/serial.py
+++ data_prep/gimms_modis_ndvi/daily_to_yearly/serial.py
@@ -24,7 +24,8 @@ out_base = "/sciclone/aiddata10/REU/data/rasters/external/global/modis_yearly"
 
 method = "max"
 
-nodata = -9999
+# nodata = -9999
+nodata = 255
 
 # --------------------
 
@@ -38,7 +39,7 @@ if method not in method_list:
 # list of years to ignore/accept
 # list of all years to process
 
-ignore = []
+ignore = ['2000']
 qlist = [name for name in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, name)) and name not in ignore]
 
 # accept = ['2000']
@@ -51,7 +52,6 @@ geotransform = None
 while c < len(qlist):
 
     year = qlist[c]
-
     print year
     
     file_paths = [data_path + "/" + year + "/" + name for name in os.listdir(data_path +"/"+ year) if not os.path.isdir(os.path.join(data_path +"/"+ year, name)) and name.endswith(".tif")]
@@ -63,33 +63,50 @@ while c < len(qlist):
         geotransform = tmp_file.GetGeoTransform()
 
 
-    year_files = [gdal.Open(f) for f in file_paths]
+    try:
+        year_files = [gdal.Open(f) for f in file_paths]
 
-    year_data = np.empty([nrows, ncols], dtype='int16')
-
-
-    for i in range(nrows):
-
-        # print str(year) +" "+ str(i)
-
-        row_read = np.array([y.GetRasterBand(1).ReadAsArray(0, i, ncols, 1) for y in year_files])
-
-        row_read[np.where(row_read == 255)] = nodata
-
-        row_data = np.max(row_read, axis=0)
-
-        year_data[i] = row_data
+        year_data = np.empty([nrows, ncols], dtype='uint8')
+    except:
+        print "error opening files"
+        c += size
+        continue
 
 
+    try:
+        for i in range(nrows):
 
-    output_path = out_base +"/"+ method +"/"+ year + ".tif"
-    output_raster = gdal.GetDriverByName('GTiff').Create(output_path, ncols, nrows, 1 , gdal.GDT_Int16)  
-    output_raster.SetGeoTransform(geotransform)  
-    srs = osr.SpatialReference()                 
-    srs.ImportFromEPSG(4326)  
-    output_raster.SetProjection(srs.ExportToWkt()) 
-    output_raster.GetRasterBand(1).SetNoDataValue(nodata)
-    output_raster.GetRasterBand(1).WriteArray(year_data)
+            # print str(year) +" "+ str(i)
+
+            row_read = np.array([y.GetRasterBand(1).ReadAsArray(0, i, ncols, 1) for y in year_files])
+
+            # row_read[np.where(row_read == 255)] = nodata
+            # row_data = np.max(row_read, axis=0)
+            # year_data[i] = row_data
+
+            row_masked = np.ma.MaskedArray(row_read, mask=row_read==nodata) 
+            row_data = row_masked.max(axis=0)
+            year_data[i] = row_data.filled(nodata)
+
+    except:
+        print "error running calc"
+        c += size
+        continue
+
+
+    try:
+        output_path = out_base +"/"+ method +"/"+ year + ".tif"
+        output_raster = gdal.GetDriverByName('GTiff').Create(output_path, ncols, nrows, 1 , gdal.GDT_Byte)  
+        output_raster.SetGeoTransform(geotransform)  
+        srs = osr.SpatialReference()
+        srs.ImportFromEPSG(4326)
+        output_raster.SetProjection(srs.ExportToWkt()) 
+        output_raster.GetRasterBand(1).SetNoDataValue(nodata)
+        output_raster.GetRasterBand(1).WriteArray(year_data)
+    except:
+        print "error writing output"
+        c += size
+        continue
 
 
     c += size
