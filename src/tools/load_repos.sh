ffcf6c8ab78632ec5fe1181ff29e78dc1707dc8d
userz <sgoodman@aiddata.org>
2016-02-23 12:32:12 -0500

    Update load_repos.sh

--- src/tools/load_repos.sh
+++ src/tools/load_repos.sh
@@ -10,18 +10,30 @@ else
 fi
 
 
-cd ~/active/extract-scripts
-if [ ! -d .git ]; then
-    git clone -b develop https://github.com/itpir/extract-scripts
+old_hash=$(md5sum ~/active/load_repos.sh)
+new_hash=$(md5sum ~/active/asdf/src/tools/load_repos.sh)
+
+if [ "$old_hash" != "$new_hash" ]; then
+
+    echo "Found new load_repos.sh ..."
+    cp  ~/active/asdf/src/tools/load_repos.sh ~/active/load_repos.sh
+    bash ~/active/load_repos.sh
+
 else
-   git pull origin master
+
+    cd ~/active/extract-scripts
+    if [ ! -d .git ]; then
+        git clone -b develop https://github.com/itpir/extract-scripts
+    else
+       git pull origin master
+    fi
+
+
+    cd ~/active/mean-surface-rasters
+    if [ ! -d .git ]; then
+        git clone -b develop https://github.com/itpir/mean-surface-rasters
+    else
+       git pull origin master 
+    fi
+
 fi
-
-
-cd ~/active/mean-surface-rasters
-if [ ! -d .git ]; then
-    git clone -b develop https://github.com/itpir/mean-surface-rasters
-else
-   git pull origin master 
-fi
-
