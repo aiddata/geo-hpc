226b94e6dad9631b4f6786faaf68f88d19d14daf
userz <sgoodman@aiddata.org>
2016-02-24 16:05:52 -0500

    Add initial autoscript with initial branch detection and config lookup.

---
+++ src/autoscript.py
@@ -0,0 +1,31 @@
+# 
+
+# --------------------------------------------------
+
+import sys
+import os
+
+branch = sys.argv[1]
+
+branch_dir = os.path.join(os.path.expanduser('~'), 'active', branch)
+
+if not os.path.isdir(branch_dir):
+    raise Exception('Branch directory does not exist')
+
+
+config_dir = os.path.join(branch_dir 'asdf', 'src', 'tools')
+sys.path.insert(0, config_dir)
+
+from config_utility import *
+
+config = BranchConfig(branch=branch)
+
+
+# --------------------------------------------------
+
+import pymongo
+
+
+client = pymongo.MongoClient(config.server)
+
+msr = client['det-test'].msr
