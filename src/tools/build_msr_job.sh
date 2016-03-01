0d84a8ee2c68d540a05769df132398aa8657e519
userz <sgoodman@aiddata.org>
2016-03-01 15:05:34 -0500

    Add job check to db update and msr job builders.

---
+++ src/tools/build_msr_job.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+branch=$1
+
+# timestamp=$2
+timestamp=$(date +%Y%m%d.%s)
+
+
+echo '=================================================='
+echo Running mean-surface-rasters job builder for branch: "$branch"
+echo Timestamp: $(date) #'('"$timestamp"')'
+echo -e "\n"
+
+
+# check if job needs to be run 
+echo "Checking for existing msr job..."
+qstat -nu $user
+
+if qstat -nu $user | grep -q 'asdf-msr-'"$branch"; then
+
+    echo "Existing job found"
+    echo -e "\n"
+
+else
+
+    echo "No existing job found."
+    echo "Building job..."
+
+    src="${HOME}"/active/"$branch"
+
+    mkdir -p "$src"/log/msr
+
+    job_path=$(mktemp)
+
+
+# NOTE: just leave this heredoc unindented
+#   sublime text is set to indent with spaces
+#   heredocs can only be indented with true tabs
+#   (can use cat <<- EOF to strip leading tabs )
+
+cat <<EOF >> "$job_path"
+
+#!/bin/tcsh
+#PBS -N asdf-msr-$branch
+#PBS -l nodes=2:c18c:ppn=16
+#PBS -l walltime=180:00:00
+#PBS -o $src/log/msr/$timestamp.msr.log
+#PBS -j oe
+
+echo 'Job id: '"$PBS_JOBID"
+
+echo -e "\n *** Running mean-surface-rasters autoscript.py... \n"
+mpirun --mca mpi_warn_on_fork 0 -np 32 python-mpi $src/asdf/src/tools/update_trackers.py $branch
+
+EOF
+
+
+    qsub "$job_path"
+
+    rm "$job_path"
+
+fi
--- src/tools/build_update_job.sh
+++ src/tools/build_update_job.sh
@@ -5,19 +5,43 @@ branch=$1
 
 timestamp=$2
 
+
+
+
 echo '=================================================='
-echo Building db update job for branch: "$branch"
+echo Running db_updates job builder for branch: "$branch"
 echo Timestamp: $(date) '('"$timestamp"')'
 echo -e "\n"
 
-src="${HOME}"/active/"$branch"
 
-job_path=$(mktemp)
+# check if job needs to be run 
+echo "Checking for existing db_updates job..."
+
+if qstat -nu $user | grep -q 'asdf-update-'"$branch"; then
+
+    echo "Existing job found"
+    echo -e "\n"
+    qstat -nu $user
+
+else
+
+    echo "No existing job found."
+    echo "Building job..."
+
+    src="${HOME}"/active/"$branch"
+
+    job_path=$(mktemp)
+
+
+# NOTE: just leave this heredoc unindented
+#   sublime text is set to indent with spaces
+#   heredocs can only be indented with true tabs
+#   (can use cat <<- EOF to strip leading tabs )
 
 cat <<EOF >> "$job_path"
 
 #!/bin/tcsh
-#PBS -N asdf-update
+#PBS -N asdf-update-$branch
 #PBS -l nodes=1:c18c:ppn=1
 #PBS -l walltime=180:00:00
 #PBS -o $src/log/db_updates/$timestamp.db_updates.log
@@ -37,7 +61,8 @@ python $src/asdf/src/tools/update_msr_list.py $branch
 EOF
 
 
-qsub "$job_path"
+    qsub "$job_path"
 
-rm "$job_path"
+    rm "$job_path"
 
+fi
