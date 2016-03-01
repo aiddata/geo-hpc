96124c16373afcbdba1aeba5a412f2288c52c72a
userz <sgoodman@aiddata.org>
2016-03-01 15:42:05 -0500

    Test changes to job logs.

--- src/tools/build_update_job.sh
+++ src/tools/build_update_job.sh
@@ -52,13 +52,13 @@ echo 'Timestamp: '$timestamp >> $output_path
 echo 'Job id: $PBS_JOBID"' >> $output_path
 
 echo -e "\n *** Running update_trackers.py... \n" >> $output_path
-python $src/asdf/src/tools/update_trackers.py $branch 2>&1 | tee 1>>$output_path
+python $src/asdf/src/tools/update_trackers.py $branch |& tee -a $output_path
 
 echo -e "\n *** Running update_extract_list.py... \n"  >> '$output_path
-python $src/asdf/src/tools/update_extract_list.py $branch 2>&1 | tee 1>>$output_path
+python $src/asdf/src/tools/update_extract_list.py $branch |& tee -a $output_path
 
 echo -e "\n *** Running update_msr_list.py... \n" >> '$output_path
-python $src/asdf/src/tools/update_msr_list.py $branch 2>&1 | tee 1>>$output_path
+python $src/asdf/src/tools/update_msr_list.py $branch |& tee -a $output_path
 
 cat $output_path >> $src/log/db_updates/$timestamp.db_updates.log
 
