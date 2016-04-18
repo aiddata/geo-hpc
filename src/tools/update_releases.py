17c51fbaa818fe5be127dd3a8b53912dd3a0be6d
userz <sgoodman@aiddata.org>
2016-04-18 14:58:27 -0400

    Debug.

--- src/tools/update_releases.py
+++ src/tools/update_releases.py
@@ -56,10 +56,11 @@ except OSError as exception:
 all_repo_releases = os.listdir(repo_dir)
 
 
-modern_str = ".*AIMS_GeocodedResearchRelease_Level1_v.*\.zip"
-modern_expr = re.compile(modern_str)
+modern_str = ".*AIMS_GeocodedResearchRelease_Level1_v.*"
+modern_expr_dir = re.compile(modern_str)
+modern_expr_zip = re.compile(modern_str+"\.zip")
 modern_repo_dirnames = [i[:-4] for i in all_repo_releases
-                        if modern_expr.match(i)]
+                        if modern_expr_zip.match(i)]
 
 
 # -----------------------------------------------------------------------------
@@ -83,7 +84,8 @@ for i in new_repo_dirnames:
 
 rtool_repo = ReleaseTools()
 rtool_repo.set_dir_releases(data_dir)
-latest_data_dirnames = [os.path.basename(i[2]) for i in rtool_repo.get_latest_releases()]
+latest_data_dirnames = [os.path.basename(i[2]) for i in rtool_repo.get_latest_releases()
+                        if modern_expr_dir.match(os.path.basename(i[2]))]
 
 
 # rtool_data = ReleaseTools()
