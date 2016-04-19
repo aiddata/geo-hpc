1af0ea2a93fb3958c60ff098ac9e84d8df8a7cbc
userz <sgoodman@aiddata.org>
2016-04-19 15:38:51 -0400

    Debug python path in crons.

--- src/tools/cron_wrapper.sh
+++ src/tools/cron_wrapper.sh
@@ -10,11 +10,13 @@ timestamp=$(date +%Y%m%d)
 # make sure log directories exist
 mkdir -p "$src"/log/{update_repos,cleanup_repos,db_updates}
 
+. "${HOME}"/.cshrc
+
 case $cron in
     "update_repos")     bash "$src"/asdf/src/tools/update_repos.sh "$branch" 2>&1 | tee 1>>"$src"/log/update_repos/"$timestamp".update_repos.log
                         exit 0;;
 
-    "cleanup_repos")     bash "$src"/asdf/src/tools/cleanup_repos.sh "$branch" 2>&1 | tee 1>>"$src"/log/cleanup_repos/"$timestamp".cleanup_repos.log
+    "cleanup_repos")    bash "$src"/asdf/src/tools/cleanup_repos.sh "$branch" 2>&1 | tee 1>>"$src"/log/cleanup_repos/"$timestamp".cleanup_repos.log
                         exit 0;;
 
     "db_updates")       bash "$src"/asdf/src/tools/build_db_updates_job.sh "$branch" "$timestamp" 2>&1 | tee 1>>"$src"/log/db_updates/"$timestamp".db_updates.log
