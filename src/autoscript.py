0aa4784a48b2daf5b5175a08d31e83d99c21cc43
userz <sgoodman@aiddata.org>
2016-04-19 18:02:24 -0400

    Debug / test msr job auto. Remove Somalia.

--- src/autoscript.py
+++ src/autoscript.py
@@ -173,8 +173,8 @@ if job.rank == 0:
 
         print 'finding request:'
         find_request = msr.find_one({
-            'hash': 'f1fdd4f63dc52132ece0bda0156d95c5dc0f2db1'#,
-            # 'status': 0
+            # 'hash': 'f1fdd4f63dc52132ece0bda0156d95c5dc0f2db1'#,
+            'status': 0
         }, sort=[("priority", -1), ("submit_time", 1), ("percentage", 1)])
 
         print find_request
