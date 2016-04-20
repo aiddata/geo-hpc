f035c48378dfa857b2ca1c1ef27ee9f77c8185af
userz <sgoodman@aiddata.org>
2016-04-20 09:57:02 -0400

    Debug / test msr job auto.

--- src/autoscript.py
+++ src/autoscript.py
@@ -60,33 +60,33 @@ job = mpi_utility.NewParallel()
 if job.rank == 0:
     import pymongo
 
-# -------------------------------------
+    # -------------------------------------
 
-# import sys
-# import os
+    # import sys
+    # import os
 
-branch = sys.argv[1]
+    branch = sys.argv[1]
 
-branch_dir = os.path.join(os.path.expanduser('~'), 'active', branch)
+    branch_dir = os.path.join(os.path.expanduser('~'), 'active', branch)
 
-if not os.path.isdir(branch_dir):
-    raise Exception('Branch directory does not exist')
-
-
-config_dir = os.path.join(branch_dir, 'asdf', 'src', 'tools')
-sys.path.insert(0, config_dir)
-
-from config_utility import BranchConfig
-
-config = BranchConfig(branch=branch)
+    if not os.path.isdir(branch_dir):
+        raise Exception('Branch directory does not exist')
 
 
-# -------------------------------------
+    config_dir = os.path.join(branch_dir, 'asdf', 'src', 'tools')
+    sys.path.insert(0, config_dir)
+
+    from config_utility import BranchConfig
+
+    config = BranchConfig(branch=branch)
 
 
-# check mongodb connection
-if config.connection_status != 0:
-    sys.exit("connection status error: " + str(config.connection_error))
+    # -------------------------------------
+
+
+    # check mongodb connection
+    if config.connection_status != 0:
+        sys.exit("connection status error: " + str(config.connection_error))
 
 
 # =============================================================================
