f865307175917674e50f3eec65b96b5899e6a921
userz <sgoodman@aiddata.org>
2016-04-26 12:41:44 -0400

    Test using only job log for db updates. Add separate job log for msr jobs.

--- src/tools/db_updates_script.sh
+++ src/tools/db_updates_script.sh
@@ -5,24 +5,24 @@ branch=$1
 timestamp=$2
 src=$3
 
-output_path=$(mktemp -p "$src"/log/db_updates/tmp)
+# output_path=$(mktemp -p "$src"/log/db_updates/tmp)
 
-echo 'Timestamp: '$timestamp >> "$output_path"
-echo 'Job id: '"$PBS_JOBID" >> "$output_path"
+echo 'Timestamp: '$timestamp #>> "$output_path"
+echo 'Job id: '"$PBS_JOBID" #>> "$output_path"
 
-echo -e "\n *** Running update_trackers.py... \n" >> "$output_path"
-python $src/asdf/src/tools/update_trackers.py "$branch" 2>&1 | tee 1>> "$output_path"
+echo -e "\n *** Running update_trackers.py... \n" #>> "$output_path"
+python $src/asdf/src/tools/update_trackers.py "$branch" #2>&1 | tee 1>> "$output_path"
 
-echo -e "\n *** Running update_extract_queue.py... \n"  >> "$output_path"
-python $src/asdf/src/tools/update_extract_queue.py "$branch" 2>&1 | tee 1>> "$output_path"
+echo -e "\n *** Running update_extract_queue.py... \n"  #>> "$output_path"
+python $src/asdf/src/tools/update_extract_queue.py "$branch" #2>&1 | tee 1>> "$output_path"
 
-echo -e "\n *** Running update_msr_queue.py... \n" >> "$output_path"
-python $src/asdf/src/tools/update_msr_queue.py "$branch" 2>&1 | tee 1>> "$output_path"
+echo -e "\n *** Running update_msr_queue.py... \n" #>> "$output_path"
+# python $src/asdf/src/tools/update_msr_queue.py "$branch" 2>&1 | tee 1>> "$output_path"
 
-echo -e "\n" >> "$output_path"
-echo $(date) >> "$output_path"
-echo -e "\nDone \n" >> "$output_path"
+echo -e "\n" #>> "$output_path"
+echo $(date) #>> "$output_path"
+echo -e "\nDone \n" #>> "$output_path"
 
-cat "$output_path" >> "$src"/log/db_updates/"$timestamp".db_updates.log
+# cat "$output_path" >> "$src"/log/db_updates/"$timestamp".db_updates.log
 
-rm "$output_path"
+# rm "$output_path"
