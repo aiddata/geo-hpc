437536807c7a86778cdc8877e7dca38a4a70ad3c
userz <sgoodman@aiddata.org>
2016-05-09 12:02:21 -0400

    Debug.

--- src/test_rasterstats/main.py
+++ src/test_rasterstats/main.py
@@ -163,8 +163,8 @@ def gen_zonal_stats(
             geom_bounds = tuple(geom.bounds)
 
             fsrc = rast.read(bounds=geom_bounds)
-            print fsrc.dtype
-            print fsrc.nbytes
+            print fsrc.array.dtype
+            print fsrc.array.nbytes
 
             # create ndarray of rasterized geometry
             rv_array = rasterize_geom(geom, like=fsrc, all_touched=all_touched)
