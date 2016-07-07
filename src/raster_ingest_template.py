223556cee8a87e490d7d5836dde2afa51d0a41cf
userz <sgoodman@aiddata.org>
2016-07-07 15:41:25 -0400

    Update raster template to actual dataset example.

--- src/raster_ingest_template.py
+++ src/raster_ingest_template.py
@@ -1,24 +1,24 @@
 {
-	"base": "/sciclone/aiddata10/REU/data/rasters/external/global/test/",
+	"base": "/sciclone/aiddata10/REU/data/rasters/external/global/gpw_v3/density",
 	"type": "raster",
 	"file_extension": ".asc",
 	"file_mask": "xxxxxxxx_YYYY.asc",
-	"name": "Test_data",
-	"title": "This is a test",
-	"description": "Raster test ingest file",
-	"version": 0.1,
+	"name": "gpw_v3_density",
+	"title": "GPW V3 Population Density",
+	"description": "Population density (UN Adjusted values) from GPW v3.",
+	"version": 3,
 	"options": {
-	    "resolution": 0.5,
-	    "extract_types": ["mean"],
+	    "resolution": 0.041667,
+	    "extract_types": ["mean", "max"],
 	    "factor": 1,
-	    "variable_description": "Stuff",
-	    "mini_name": "abcd"
+	    "variable_description": "Population density per square km",
+	    "mini_name": "g3du"
 	},
 	"extras": {
-	    "citation": "cite",
-	    "sources_web": "www.google.com",
-	    "sources_name": "Google",
-	    "tags": ["google", "data"]
+	    "citation": "Center for International Earth Science Information Network - CIESIN - Columbia University, and Centro Internacional de Agricultura Tropical - CIAT. 2005. Gridded Population of the World, Version 3 (GPWv3): Population Density Grid. Palisades, NY: NASA Socioeconomic Data and Applications Center (SEDAC). http://dx.doi.org/10.7927/H4XK8CG2.",
+	    "sources_web": "http://sedac.ciesin.columbia.edu/data/set/gpw-v3-population-density",
+	    "sources_name": "CIESIN",
+	    "tags": ["population", "density", "ciesin"]
 	},
 	"active": 1
 }
