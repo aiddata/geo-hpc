0e310c7b34cfdcabc312be0dcff0da9116b3c53a
userw <sgoodman@aiddata.org>
2016-07-24 09:08:06 -0400

    Move mpi util script to asdf repo and update autoscript. Cleanup misc.

--- src/autoscript.py
+++ src/autoscript.py
@@ -47,32 +47,7 @@ from affine import Affine
 from msr_utility import CoreMSR, MasterStack
 
 
-# =============================================================================
-# =============================================================================
-# basic init and find request
-
-# from mpi4py import MPI
-import mpi_utility
-
-job = mpi_utility.NewParallel()
-
-request = 0
-
-
-def get_version():
-    vfile = os.path.join(os.path.dirname(__file__), "_version.py")
-    with open(vfile, "r") as vfh:
-        vline = vfh.read()
-    vregex = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    match = re.search(vregex, vline, re.M)
-    if match:
-        return match.group(1)
-    else:
-        raise RuntimeError(
-            "Unable to find version string in {}.".format(vfile))
-
-
-# -------------------------------------
+# -----------------------------------------------------------------------------
 
 import sys
 import os
@@ -99,12 +74,23 @@ if config.connection_status != 0:
 
 
 # -------------------------------------------------------------------------
-# -------------------------------------------------------------------------
-# find request
 
-client = config.client
-c_asdf = client.asdf.data
-c_msr = client.asdf.msr
+import mpi_utility
+job = mpi_utility.NewParallel()
+
+
+def get_version():
+    vfile = os.path.join(os.path.dirname(__file__), "_version.py")
+    with open(vfile, "r") as vfh:
+        vline = vfh.read()
+    vregex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    match = re.search(vregex, vline, re.M)
+    if match:
+        return match.group(1)
+    else:
+        raise RuntimeError(
+            "Unable to find version string in {}.".format(vfile))
+
 
 tmp_v1 = config.versions["mean-surface-rasters"]
 tmp_v2 = get_version()
@@ -115,8 +101,16 @@ else:
     raise Exception("Config and src versions do not match")
 
 
+client = config.client
+c_asdf = client.asdf.data
+c_msr = client.asdf.msr
+
 general_output_base = '/sciclone/aiddata10/REU/outputs/' + branch + '/msr'
 
+
+# -----------------------------------------------------------------------------
+
+request = 0
 
 if job.rank == 0:
 
