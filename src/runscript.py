d4b07a96200a68db78ec75da4054624763d85dc4
userz <sgoodman@aiddata.org>
2016-07-25 11:09:27 -0400

    Fix mpi util import in runscript.

--- src/runscript.py
+++ src/runscript.py
@@ -9,8 +9,27 @@ import time
 import json
 
 import extract_utility
-import mpi_utility
 
+
+
+# -----------------------------------------------------------------------------
+
+import sys
+import os
+
+branch = sys.argv[1]
+
+branch_dir = os.path.join(os.path.expanduser('~'), 'active', branch)
+
+if not os.path.isdir(branch_dir):
+    raise Exception('Branch directory does not exist')
+
+
+config_dir = os.path.join(branch_dir, 'asdf', 'src', 'tools')
+sys.path.insert(0, config_dir)
+
+
+import mpi_utility
 job = mpi_utility.NewParallel()
 
 
