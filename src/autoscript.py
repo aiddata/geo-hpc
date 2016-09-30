ce8ec9f51ed0bc2fbff6d40ad0339e2e6c21e2ae
userz <sgoodman@aiddata.org>
2016-09-30 14:52:42 -0400

    Debug.

--- src/autoscript.py
+++ src/autoscript.py
@@ -42,32 +42,33 @@ from shapely.geometry import shape, box
 
 from msr_utility import CoreMSR, MasterStack
 
-import mpi_utility
-job = mpi_utility.NewParallel()
 
 
 # -----------------------------------------------------------------------------
+
+import sys
+import os
+
+branch = sys.argv[1]
+
+branch_dir = os.path.join(os.path.expanduser('~'), 'active', branch)
+
+if not os.path.isdir(branch_dir):
+    raise Exception('Branch directory does not exist')
+
+
+config_dir = os.path.join(branch_dir, 'asdf', 'src', 'tools')
+sys.path.insert(0, config_dir)
+
+from config_utility import BranchConfig
+
+import mpi_utility
+job = mpi_utility.NewParallel()
 
 
 config = 0
 
 if job.rank == 0:
-
-    import sys
-    import os
-
-    branch = sys.argv[1]
-
-    branch_dir = os.path.join(os.path.expanduser('~'), 'active', branch)
-
-    if not os.path.isdir(branch_dir):
-        raise Exception('Branch directory does not exist')
-
-
-    config_dir = os.path.join(branch_dir, 'asdf', 'src', 'tools')
-    sys.path.insert(0, config_dir)
-
-    from config_utility import BranchConfig
 
     config = BranchConfig(branch=branch)
 
