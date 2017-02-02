5fd9b037488105519c31764ad00e678d599ec5ed
sgoodm <sgoodman@aiddata.org>
2017-02-02 13:56:17 -0500

    Debug

--- queue/request_tools.py
+++ queue/request_tools.py
@@ -651,6 +651,7 @@ class QueueToolBox():
                         c[len("exfield_"):])
 
                 elif result_field.endswith('reliability') or result_field.startswith('worldbank'):
+                    print "\n###\n{0}\n###".format(result_field)
                     tmp_split = result_field.split('.')
                     tmp_field = "{0}.{1}.{2}".format(
                         tmp_split[0],
