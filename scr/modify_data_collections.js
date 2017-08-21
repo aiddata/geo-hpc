10eb214f420045035fa8d8a20337a6d0220b122d
userz <sgoodman@aiddata.org>
2017-08-21 16:03:50 -0400

    Fix mongo snippet for final usage

--- scr/modify_data_collections.js
+++ scr/modify_data_collections.js
@@ -1,6 +1,6 @@
 //  modify asdf->data collection to bulk change base paths
 
-db.data.find({}, {base:1}).forEach(function(doc){
+db.data.find({}).forEach(function(doc){
 
     base = doc['base']
 
@@ -10,9 +10,18 @@ db.data.find({}, {base:1}).forEach(function(doc){
     new_base = base.replace(old_root, new_root)
 
     final_base = new_base.replace('external/global/', '')
-    print(final_base)
+    // print(final_base)
 
-    doc['base'] = final_base;
-    db.data.save(doc);
+    doc['base'] = final_base
+    db.data.save(doc)
+
+})
+
+
+
+db.data.find({}, {base:1}).forEach(function(doc){
+
+    base = doc['base']
+    print(base)
 
 })
