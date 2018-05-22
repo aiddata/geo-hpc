60cd4fea817f871a389a716f5170c05e6fb31829
userz <sgoodman@aiddata.org>
2018-05-22 13:14:30 -0400

    Fix permissions tasks for home dir

--- tasks/set_permissions.sh
+++ tasks/set_permissions.sh
@@ -2,8 +2,14 @@
 
 branch=$1
 
-find "${HOME}" -type d -exec chmod u=rwx,g=rxs,o=rx {} +
-find "${HOME}" -type f -exec chmod u=rw,g=r,o=r {} +
+# find "${HOME}" -type d -exec chmod u=rwx,g=rxs,o= {} +
+# find "${HOME}" -type f -exec chmod u=rw,g=r,o= {} +
+
+find "${HOME}/privatemodules" -type d -exec chmod u=rwx,g=rxs,o=rx {} +
+find "${HOME}/privatemodules" -type f -exec chmod u=rw,g=r,o=r {} +
+
+find "${HOME}/usr" -type d -exec chmod u=rwx,g=rxs,o=rx {} +
+find "${HOME}/usr" -type f -exec chmod u=rw,g=r,o=r {} +
 
 find "/sciclone/aiddata10/geo/${branch}" -type d -exec chmod u=rwx,g=rxs,o=rx {} +
 find "/sciclone/aiddata10/geo/${branch}" -type f -exec chmod u=rw,g=r,o=r {} +
