2cf5d1715cbc1b3c737e57775de8d7fd330c7970
userz <sgoodman@aiddata.org>
2018-07-11 09:47:19 -0400

    Add quick extract script to scr and update featured bounds

--- scr/quick_extract.py
+++ scr/quick_extract.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 
-vector = "/home/userz/git/afghanistan-gie/canal_data/canal_point_grid.geojson"
+vector = "/home/userz/git/afghanistan_gie/canal_data/canal_point_grid.geojson"
 raster = "/sciclone/aiddata10/REU/projects/afghanistan_gie/distance_to_canals/distance_starts.tif"
 
 output = "/sciclone/aiddata10/REU/projects/afghanistan_gie/extract_data/canal_starts_grid_extract.csv"
