ac70be2d49112798b2f6fcb4d1b4dd9645f9a29e
userz <sgoodman@aiddata.wm.edu>
2019-02-26 16:31:45 -0500

    Make quick extract scr script generic

--- scr/quick_extract.py
+++ scr/quick_extract.py
@@ -6,11 +6,10 @@ import pandas as pd
 
 
 
-vector = "/home/userz/git/afghanistan_gie/canal_data/canal_point_grid.geojson"
-raster = "/sciclone/aiddata10/REU/projects/afghanistan_gie/distance_to_canals/distance_starts.tif"
+vector = "/path/to/some/vector.geojson"
+raster = "/sciclone/aiddata10/REU/some/raster.tif"
 
-output = "/sciclone/aiddata10/REU/projects/afghanistan_gie/extract_data/canal_starts_grid_extract.csv"
-
+output = "/path/to/results.csv"
 
 
 
