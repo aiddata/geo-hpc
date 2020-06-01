f7543e5fc432b7c6a16bc1d74152a5ddfc2bbaca
sgoodm <sgoodman@aiddata.wm.edu>
2020-06-01 14:07:45 -0400

    Fix typo

--- mongodb/mongo_repair.sh
+++ mongodb/mongo_repair.sh
@@ -6,7 +6,7 @@ sudo rm /var/lib/mongodb/mongod.lock
 
 # only repair if needed, may take a while to run on larger
 # collection like features db
-sudo mongod --repair --dbpath /var/lib/mongodb --storageEnginer wiredTiger
+sudo mongod --repair --dbpath /var/lib/mongodb --storageEngine wiredTiger
 
 # files need proper ownership
 sudo chown -R mongodb:mongodb /var/lib/mongodb
